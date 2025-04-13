e163749986525fb56e154207727334fea2bf5093
Rita Potapkina <rita26-10@mail.ru>
2025-04-13 11:58:17 +0400

    feat : add basic py and cpp templates

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <array>
+#include <random>
+#include <ctime>
+#include <climits>
+#include <cassert>
+
+#define pb push_back
+
+using namespace std;
+
+typedef long long li;
+typedef long double ld;
+typedef pair<int, int> ptt;
+
+//ifstream in("input.in");
+//ofstream out("output.out");
+
+const int INF = 1e9;
+const int M = 998244353;
+
+void solve() {
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cout << setprecision(15) << fixed;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
